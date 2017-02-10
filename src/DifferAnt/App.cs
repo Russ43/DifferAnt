611eb8cbe323d646198c6b1c69f2a75e80ffbc1e
Russell <russ@company43.com>
2017-02-09 16:03:15 -0800

    removed junk from project template

---
+++ src/DifferAnt/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DifferAnt
+{
+    internal class App
+    {
+        [STAThread]
+        static private void Main()
+        {
+            Application application = new Application();
+            application.Run(new DifferAntWindow());
+        }
+    }
+}
